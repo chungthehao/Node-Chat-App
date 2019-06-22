ceb39842f23b150a41191b442ac8e76c20a060e5
thehao <chungthehao@gmail.com>
2019-06-22 22:34:24 +0700

    Socket.io Events, a little counter app

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io()
+const socket = io() // Initialize the connection
+
+// * Receive event từ server
+socket.on('countUpdated', (count) => {
+    console.log('The count has been updated!', count)
+})
+
+// * Send event tới server
+document.querySelector('#increment').addEventListener('click', e => {
+    console.log('Clicked!')
+    socket.emit('increment')
+})
--- src/index.js
+++ src/index.js
@@ -12,9 +12,23 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
+let count = 0
 // * Chạy MỖI khi có 1 kết nối mới (listening for a 'connection' event to occur)
-io.on('connection', () => {
+io.on('connection', (socket) => { // 'socket' is an object, chứa info về cái new connection
     console.log('New Websocket connection')
+
+    // * Khi dùng socket.io để transfering data -> mình đang sending & receiving cái gọi là event
+    // --> Ở đây là send event từ server và receive event từ client
+    socket.emit('countUpdated', count) // Từ tham số thứ 2 trở đi, sẽ là đầu vào của callback func (event handler) bên client, thứ tự của các tham số là quan trọng!
+
+    socket.on('increment', () => {
+        count++
+
+        // * Thông báo tới 1 specific client đó
+        //socket.emit('countUpdated', count) 
+        // * Emit event cho tất cả các connection hiện tại
+        io.emit('countUpdated', count)
+    })
 })
 
 server.listen(port, () => {
