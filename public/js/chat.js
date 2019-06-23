20ff2285b551db954ff3afe38777446b8094f8dc
thehao <chungthehao@gmail.com>
2019-06-23 22:16:48 +0700

    Socket.io Events Challenge

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,22 @@
 const socket = io() // Initialize the connection
 
-// * Receive event từ server
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count)
+socket.on('message', (message) => {
+    console.log(message)
 })
 
-// * Send event tới server
-document.querySelector('#increment').addEventListener('click', e => {
-    console.log('Clicked!')
-    socket.emit('increment')
+document.querySelector('#message-form').addEventListener('submit', e => {
+    e.preventDefault()
+    const msg = e.target.elements.msg.value // e.target (là cái form), e.target.elements những eles trong form đó,  e.target.elements.msg cái thằng có name là 'msg'
+    socket.emit('sendMessage', msg)
 })
+
+// * Receive event từ server
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated!', count)
+// })
+
+// * Send event tới server
+// document.querySelector('#increment').addEventListener('click', e => {
+//     console.log('Clicked!')
+//     socket.emit('increment')
+// })
